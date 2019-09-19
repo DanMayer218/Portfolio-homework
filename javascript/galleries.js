f06926c39edfe01f0f4be5e8bad9be836100b660
Dans Bootcamp <dansbootcamp@AmbersImac.home>
2019-09-19 02:20:24 -0400

    much closer to the kind of gallery I wanted to create. though I am having issues with the javascript inacting the function of being able to zoom all the way in kind of like a lightbox, off to bed and will try to pick it up in the morning

--- javascript/galleries.js
+++ javascript/galleries.js
@@ -1,2 +1,11 @@
 // This javascript file is exclusive to javascript functions and methods surrounding gallery pages
-$('.carousel').carousel()
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, options);
+  });
+
+  var instance = M.Materialbox.getInstance(elem);
+
+  instance.open();
+  instance.close();
+  
